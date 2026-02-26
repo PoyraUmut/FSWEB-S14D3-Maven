6d0755f897210c8a7731091ac3227366dc0a77ca
Umut Tekin <tekinumut9@gmail.com>
2026-02-26 08:03:12 +0300

    Sprint 14 Gün 3 Projesi Tamamlandı.

---
+++ src/main/java/org/example/arge/HybridCar.java
@@ -0,0 +1,39 @@
+package org.example.arge;
+
+public class HybridCar extends CarSkeleton {
+
+    private double avgKmPerLitre;
+    private int batterySize;
+    private int cylinders;
+
+    public HybridCar(String name, String description,
+                     double avgKmPerLitre, int batterySize, int cylinders) {
+        super(name, description);
+        this.avgKmPerLitre = avgKmPerLitre;
+        this.batterySize = batterySize;
+        this.cylinders = cylinders;
+    }
+
+    public double getAvgKmPerLitre() {
+        return avgKmPerLitre;
+    }
+
+    public int getBatterySize() {
+        return batterySize;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println("Hybrid system starting both gas and electric engines");
+    }
+
+    @Override
+    public void drive() {
+        System.out.println("Driving hybrid car efficiently");
+        super.drive();
+    }
+}
